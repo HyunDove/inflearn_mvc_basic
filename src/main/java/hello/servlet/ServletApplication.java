a085bdfa9cca3ae711f1ff3ef94fd629e258981f
김승현 <dsagom@gmail.com>
2024-04-21 17:51:01 +0900

    section 2 : 서블릿

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServletApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
